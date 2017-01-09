de23f2de1348849a3c642a1b72eb2364dbcad4a4
claudio <letresorinterdit@gmail.com>
2017-01-09 21:12:30 +0000

    Tutorial Spring-Core Spring Hello World

---
+++ SpringCore/src/main/java/com/claudio/SpringCore/App.java
@@ -0,0 +1,13 @@
+package com.claudio.SpringCore;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
